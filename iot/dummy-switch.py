608061ba281bdc07ae58edbe926b0945568a6ee9
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-26 13:10:47 +0200

    Moved the IOTDevice to the iot folder. Since there can be several types of devices for each type (dummy-switch, intel-switch...), it is better to have the class implemented by the script for each device.

--- iot/dummy-switch.py
+++ iot/dummy-switch.py
@@ -1,6 +1,112 @@
 import argparse
-from ..classes import SwitchDevice
 import sys
+import random
+from enum import Enum
+
+from .IOTDevice import IOTDevice
+
+class SwitchState(Enum):
+	"""
+	Enum class to represent the state of the switch.
+	"""
+	PAYLOAD_ON = "ON"
+	PAYLOAD_OFF = "OFF"
+
+class DummySwitchDevice(IOTDevice):
+	"""
+	Class that represents a switch device.
+	"""
+	def __init__(
+		self,
+		host: str,
+		port: int,
+		device_id: str,
+		probability=0.3: float
+	):
+		"""
+		Constructor of the DummySwitchDevice class.
+		Args:
+			host (str): Host of the MQTT broker.
+			port (int): Port of the MQTT broker.
+			device_id (str): ID of the device.
+			probability (float): Probability of the switch failing.
+		"""
+		# Save the main device params
+		super().__init__(host, port, device_id)
+
+		# Save the switch probability
+		self.probability = probability
+
+		# Set the switch init state
+		self.status = SwitchState.PAYLOAD_OFF
+
+		# Set the function
+		self.client.on_message = self.on_message
+		self.client.on_connect = self.on_connect
+
+		# Device header, for debugging
+		self.device_header = f"[Switch - {self.device_id}]"
+	
+	def on_connect(self, client, userdata, flags, rc) -> None:
+		"""
+		Callback function that is called when the client connects to the broker.
+		Args:
+			client (mqtt.Client): The client instance for this callback.
+			userdata (any): The private user data as set in Client() or userdata_set().
+			flags (dict): Response flags sent by the broker.
+			rc (int): The connection result.
+		"""
+		# Debud print
+		print(f"{self.device_header} Connected with result code {rc}")
+
+		# Subscribe to the command topic
+		self.client.subscribe(self.command_topic)
+
+		# Publish the current state
+		self.client.publish(
+			self.status_topic,
+			self.status.value,
+		)
+
+	def on_message(self, client, userdata, msg) -> None:
+		"""
+		Callback function that is called when a message is received.
+		Args:
+			client (mqtt.Client): The client instance for this callback.
+			userdata (any): The private user data as set in Client() or userdata_set.
+			msg (mqtt.MQTTMessage): An instance of MQTTMessage. This is a class with
+				two members: topic and payload. The payload is a bytes object and can
+				be decoded by using the decode() method.
+		"""
+		# Get the message payload
+		payload = msg.payload.decode().strip().upper()
+		print(f"{self.device_header} Received message: {payload}")
+
+		# Get the status from the enum
+		try:
+			received_status = SwitchState(payload)
+		except ValueError:
+			print(f"{self.device_header} Invalid status received, ignoring")
+			received_status = None
+		
+		# If the status is valid, try to change it
+		if received_status:
+			random_number = random.randint(0, 100) / 100
+
+			# Simulate a failure
+			if random_number < self.probability:
+				print(f"{self.device_header} Simulating a failure")
+			elif received_status == self.status:
+				print(f"{self.device_header} Status is already {self.status.value}")
+			else:
+				print(f"{self.device_header} Changing status from {self.status.value} to {received_status.value}")
+				self.status = received_status
+		
+		# Publish the current state
+		self.client.publish(
+			self.status_topic,
+			self.status.value,
+		)
 
 def parse_params() -> argparse.Namespace:
 	"""
@@ -47,7 +153,7 @@ def main():
 	args = parse_params()
 
 	# Init switch
-	device = SwitchDevice(
+	device = DummySwitchDevice(
 		host=args.host,
 		port=args.port,
 		device_id=args.id,
