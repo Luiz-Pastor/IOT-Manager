d5e1cf41a142c29095c9b3d1a6bd2634a075126e
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-30 00:02:55 +0200

    Rendered the index template (some changes should be done)

--- iot-manager/app/views.py
+++ iot-manager/app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello, world! This is the IoT Manager app.')
+	return render(request, 'index.html')
