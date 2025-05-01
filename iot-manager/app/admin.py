22f9cd042796fbc87f01ddcf01c66e1fe408f85f
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-01 18:39:36 +0200

    Added to the admin page the models, to manage them easily

--- iot-manager/app/admin.py
+++ iot-manager/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DummySensor, DummyClock, DummySwitch
 
 # Register your models here.
+admin.site.register(DummySensor)
+admin.site.register(DummyClock)
+admin.site.register(DummySwitch)
