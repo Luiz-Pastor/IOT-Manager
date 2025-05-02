5f98d8d68621745ac8ba9ff5cb1aba23e3d683c0
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-03 01:45:02 +0200

    Added a field to the device that send the log, and added to the template. Also change the way to order the logs

--- iot-manager/app/models/log.py
+++ iot-manager/app/models/log.py
@@ -4,14 +4,24 @@ class Log(models.Model):
 	"""
 	Class to save the devices logs
 	"""
-	# Log message
-	message = models.TextField()
-
 	# Date when the log was done
-	timestamp = models.DateTimeField(auto_now_add=True)
+	timestamp = models.DateTimeField(
+		auto_now_add=True
+	)
+
+	# Device name
+	device = models.CharField(
+		max_length=128,
+		null=False, blank=False
+	)
+
+	# Log message
+	message = models.TextField(
+		null=False, blank=False
+	)
 
 	class Meta:
-		ordering = ['timestamp']
+		ordering = ['-timestamp']
 
 	def __str__(self):
 		return f"[{self.timestamp:%Y-%m-%d %H:%M:%S}] {self.message}"
