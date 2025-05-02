f57e194eb0054d661603e5fae4e98d1d7d0c81f1
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-02 22:32:02 +0200

    Added the rule model to the admin page (so the rule creation can be debugged better than other ways)

--- iot-manager/app/admin.py
+++ iot-manager/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import DummySensor, DummyClock, DummySwitch
+from .models import DummySensor, DummyClock, DummySwitch, Rule
 
 # Register your models here.
 admin.site.register(DummySensor)
 admin.site.register(DummyClock)
 admin.site.register(DummySwitch)
+admin.site.register(Rule)
