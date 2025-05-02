c2568875ced75b052f9984d21e0f92b296c2313d
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-02 22:32:29 +0200

    Implemented the rule form, with some widgets to optimize the creation (and visual)

--- iot-manager/app/forms.py
+++ iot-manager/app/forms.py
@@ -1,6 +1,6 @@
 # devices/forms.py
 from django import forms
-from .models import DummySensor, DummyClock, DummySwitch
+from .models import DummySensor, DummyClock, DummySwitch, Rule
 
 class DummySensorForm(forms.ModelForm):
     class Meta:
@@ -16,3 +16,19 @@ class DummySwitchForm(forms.ModelForm):
     class Meta:
         model = DummySwitch
         fields = ["id", "host", "port", "probability"]
+
+class RuleForm(forms.ModelForm):
+    class Meta:
+        model = Rule
+        fields = ['name', 'source_device', 'operator', 'threshold', 'target_device', 'command_payload']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'source_device': forms.Select(attrs={'class': 'form-select'}),
+            'operator': forms.Select(attrs={'class': 'form-select'}),
+            'threshold': forms.TextInput(attrs={'class': 'form-control'}),
+            'target_device': forms.Select(attrs={'class': 'form-select'}),
+            'command_payload': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        help_texts = {
+            'command_payload': 'Debe incluir al menos el campo "cmd", e.j. {"cmd":"set","state":"ON"}',
+        }
