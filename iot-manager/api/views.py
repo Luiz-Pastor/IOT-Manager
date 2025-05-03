8029e2156b950166554da9aa9d60ee3cfc734ef5
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-04 00:25:58 +0200

    Implemented the views to list the rules, and the one to create logs

--- iot-manager/api/views.py
+++ iot-manager/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from app.models import (
 	Device,
 	Rule,
@@ -12,5 +12,29 @@ from app.serializers import (
 
 # Create your views here.
 class DeviceViewSet(viewsets.ReadOnlyModelViewSet):
+	"""
+	API endpoint that allows devices to be viewed.
+		· GET: /devices: returns all the devices data
+	"""
 	queryset = Device.objects.all()
 	serializer_class = DeviceSerializer
+
+class RuleViewSet(viewsets.ReadOnlyModelViewSet):
+	"""
+	API endpoint that allows rules to be viewed.
+		· GET: /rules: returns all the rules data
+	"""
+	queryset = Rule.objects.all()
+	serializer_class = RuleSerializer
+
+class LogViewSet(
+	mixins.CreateModelMixin,	# Create the logs
+	viewsets.GenericViewSet,
+):
+	"""
+	API endpoint that allows logs to be created.
+		· POST: /logs: create the new log
+	"""
+	queryset = Log.objects.all()
+	serializer_class = LogSerializer
+	http_method_names = ['post']
