34d324d480c634b8fbed28ba20f4dd5adcb6e329
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-04 00:21:11 +0200

    Implemented the device view, using the app serializers

--- iot-manager/api/views.py
+++ iot-manager/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from app.models import (
+	Device,
+	Rule,
+	Log
+)
+from app.serializers import (
+	DeviceSerializer,
+	RuleSerializer,
+	LogSerializer
+)
 
 # Create your views here.
+class DeviceViewSet(viewsets.ReadOnlyModelViewSet):
+	queryset = Device.objects.all()
+	serializer_class = DeviceSerializer
