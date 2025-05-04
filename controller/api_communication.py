631249811c542e7b7b5b17e2773f0f2da9459877
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-04 20:10:42 +0200

    Added a function to create logs and send them to the API

--- controller/api_communication.py
+++ controller/api_communication.py
@@ -35,3 +35,33 @@ def get_rules_from_api(
 		return request.json()
 	except Exception:
 		return None
+
+def add_log_message(
+	host: str,
+	port:str,
+	message: str,
+	device_id: str = None
+) -> None:
+	"""
+	Function to add a log message to the api
+
+	Args:
+		host (str): Host of the Django server.
+		port (int): Port of the Django server.
+		message (str): Message to be logged.
+		device_id (str): ID of the device that generated
+			the message. This argument can be None, so the
+			log will not have a device associated.
+	"""
+	# Set the body to send
+	body = { "message": message }
+	if device_id:
+		body["device"] = device_id
+
+	try:
+		request = requests.post(
+			f"{host}:{port}/api/v1/logs/",
+			data=body
+		)
+	except Exception:
+		return None
